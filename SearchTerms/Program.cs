6e6138b9f3fc586b5f00d8d168134dd03abbf280
Chris Cullen <christopher.cullen2020@gmail.com>
2023-11-01 21:22:00 +0000

    First commit

---
+++ SearchTerms/Program.cs
@@ -0,0 +1,35 @@
+﻿// Read file using StreamReader. Reads file line by line
+using static System.Net.Mime.MediaTypeNames;
+int termsFound = 0;
+int linesInDataFile = 0;
+
+using (StreamReader searchTermsFile = new StreamReader("C:\\ScreenTest\\SearchTerms.txt"))
+{
+    string searchTerm;
+
+    while ((searchTerm = searchTermsFile.ReadLine()) != null)
+    {
+        using (StreamReader dataFile = new StreamReader("C:\\ScreenTest\\Data.txt"))
+        {
+            string dataLine;
+            linesInDataFile = 0;
+
+            while ((dataLine = dataFile.ReadLine()) != null)
+            {
+                if (dataLine.Contains(searchTerm))
+                {
+                    termsFound++;
+                }
+                linesInDataFile++;
+            }
+            dataFile.Close();
+        }
+    }
+    searchTermsFile.Close();
+}
+
+Console.WriteLine(termsFound);
+
+Console.WriteLine(linesInDataFile);
+
+Console.WriteLine(termsFound/linesInDataFile);
